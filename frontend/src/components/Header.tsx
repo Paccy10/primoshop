ccbee444443ab1ff1a602b7b889087d4016aca6d
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-05-10 18:22:57 +0200

    feature[frontend]: Header and Footer - Add react bootstrap and react icons - Add header component - Add footer component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">PrimoShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link>
+                <FaUser /> SignIn
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
