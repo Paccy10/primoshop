e77dd71009a6e5942832f988b915045e41d8f7a1
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-05-15 14:52:23 +0200

    [feature][backend]: User login: - Check if user exists - Generate jwt token - Add jwt token to cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,9 @@ connectDB();
 const port = process.env.PORT || 5000;
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("API is running...");
 });
--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,39 +1,63 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model";
+import { LoginInput } from "../interfaces/user.interface";
 
-export const loginUser = (req: Request, res: Response) => {
-  res.send("login user");
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body as LoginInput;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.comparePassword(password))) {
+    const token = jwt.sign({ userId: user._id }, `${process.env.SECRET_KEY}`, {
+      expiresIn: "30d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  }
+
+  res.status(401);
+  throw new Error("Invalid email or password");
 };
 
-export const registerUser = (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response) => {
   res.send("register user");
 };
 
-export const logoutUser = (req: Request, res: Response) => {
+export const logoutUser = async (req: Request, res: Response) => {
   res.send("logout user");
 };
 
-export const getUserProfile = (req: Request, res: Response) => {
+export const getUserProfile = async (req: Request, res: Response) => {
   res.send("get user profile");
 };
 
-export const updateUserProfile = (req: Request, res: Response) => {
+export const updateUserProfile = async (req: Request, res: Response) => {
   res.send("update user profile");
 };
 
 // Admin routes
-export const getUsers = (req: Request, res: Response) => {
+export const getUsers = async (req: Request, res: Response) => {
   res.send("get all users");
 };
 
-export const getUserById = (req: Request, res: Response) => {
+export const getUserById = async (req: Request, res: Response) => {
   res.send("get user by id");
 };
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
   res.send("delete user");
 };
 
-export const updateUser = (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
   res.send("update user");
 };
