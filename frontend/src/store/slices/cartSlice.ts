4c2612c70c1857f24e59a94704b614f5c493cfcb
Pacifique Clement Ndayisenga <32579116+Paccy10@users.noreply.github.com>
2023-05-15 12:43:38 +0200

    [feature][frontend]: Cart screen: - Add cart screen - Add change quantity on cart screen - Add remove item from cart - Add products images

--- frontend/src/store/slices/cartSlice.ts
+++ frontend/src/store/slices/cartSlice.ts
@@ -38,7 +38,12 @@ export const cartSlice = createSlice({
 
       return updateCart(state);
     },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
